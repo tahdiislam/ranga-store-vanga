226d23529755ca54266572b8773282be1a1ff530
tahdi-islam <tshdi60@gmail.com>
2022-09-12 13:39:45 +0600

    all problem in cart

--- js/app.js
+++ js/app.js
@@ -92,17 +92,21 @@ const setInnerText = (id, value) => {
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
    const priceConverted = getInputValue('price');
-   if (priceConverted > 200) {
-      setInnerText('delivery-charge', 30);
-      setInnerText('total-tax', priceConverted * 0.2);
-   }
-   if (priceConverted > 400) {
-      setInnerText('delivery-charge', 50);
-      setInnerText('total-tax', priceConverted * 0.3);
-   }
    if (priceConverted > 500) {
-      setInnerText('delivery-charge', 60);
-      setInnerText('total-tax', priceConverted * 0.4);
+     setInnerText("delivery-charge", 60);
+     setInnerText("total-tax", priceConverted * 0.4);
+   }
+   else if(priceConverted > 400) {
+     setInnerText("delivery-charge", 50);
+     setInnerText("total-tax", priceConverted * 0.3);
+   }
+    else if (priceConverted > 200) {
+     setInnerText("delivery-charge", 30);
+     setInnerText("total-tax", priceConverted * 0.2);
+   }
+   else{
+     setInnerText("delivery-charge", 20);
+     setInnerText("total-tax", priceConverted * 0.2);
    }
 };
 
